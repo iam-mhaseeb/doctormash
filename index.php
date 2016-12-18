8b4278c4162fd7f6b637cd6bb8ddbc0029daae51
Muhammad Haseeb <haseeb.emailbox@gmail.com>
2016-12-18 15:13:00 +0500

    header and footer created

--- index.php
+++ index.php
@@ -1,20 +1,44 @@
-/* 
- Under Licence HSB
- */
- 
- <!DOCTYPE html>
+  <!DOCTYPE html>
   <html>
     <head>
       <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/style.css" />
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <title>Doctor Mash - Find a Doctor</title>
     </head>
 
     <body>
+        <nav>
+            <div class="nav-wrapper">
+              <a href="#" class="brand-logo">Doctor Mash</a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">About</a></li>
+                <li><a href="Register.php">Register</a></li>
+                <li><a href="Login.phpl">Login</a></li>
+              </ul>
+            </div>
+          </nav>
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Doctor Mash</h5>
+                <p class="grey-text text-lighten-4">We help you to find best doctor.</p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2016 Copyright Doctor Mash
+            <a class="grey-text text-lighten-4 right" href="https://github.com/iam-mhaseeb/doctormash">View Source</a>
+            </div>
+          </div>
+        </footer>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
