a13f83376a35a27b6e1cec58da2ac03d7b4abadc
Muhammad Haseeb <haseeb.emailbox@gmail.com>
2016-12-18 17:30:32 +0500

    Few More

--- index.php
+++ index.php
@@ -17,12 +17,114 @@
             <div class="nav-wrapper">
               <a href="#" class="brand-logo">Doctor Mash</a>
               <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#">About</a></li>
+                  <li><a href="index.php">Home</a></li>
                 <li><a href="Register.php">Register</a></li>
                 <li><a href="Login.phpl">Login</a></li>
               </ul>
             </div>
           </nav>
+        <br>
+          <div class="slider" >
+            <ul class="slides">
+              <li>
+                <div class="caption center-align">
+                  <h3>What is Doctor Mash ?</h3>
+                  <p class="light grey-text text-lighten-3">We help you to find best doctors for you.</p>
+                </div>
+              </li>
+              <li>
+                <div class="caption center-align">
+                  <h3>Why us ?</h3>
+                  <p class="light grey-text text-lighten-3">We have intelligent database and bots which fetch best doctor details for you.</p>
+                </div>
+              </li>
+              <li>
+                <div class="caption center-align">
+                  <h3>Who we are ?</h3>
+                  <p class="light grey-text text-lighten-3">We are dedicated team trying to make finding doctor easier.</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        <div class="container" align="center">
+                <div class="main" align="center">
+                <div class="chip">
+                    Cardiology
+                </div>
+                 <div class="chip">
+                    Dentistry
+                </div>
+                 <div class="chip">
+                    Dermatology
+                </div>
+                 <div class="chip">
+                    Dietetics
+                </div>
+                 <div class="chip">
+                    Endocrinologist
+                    </div>
+                    <div class="chip">
+                    ENT
+                    </div>
+                  <div class="chip">
+                    Eye
+                    </div>
+                    <div class="chip">
+                    Gateronologist
+                    </div>
+                    <div class="chip">
+                    Genral
+                    </div>
+                    <div class="chip">
+                    Gynecology
+                    </div>
+                    <div class="chip">
+                    Homepath
+                    </div>
+                    <div class="chip">
+                    Liver
+                    </div>
+                    <div class="chip">
+                    Nephrology
+                    </div>
+                    <div class="chip">
+                    Neurology
+                    </div>
+                    <div class="chip">
+                    Oncology
+                    </div>
+                    <div class="chip">
+                    Orthapedic
+                    </div>
+                    <div class="chip">
+                    Pathology
+                    </div>
+                    <div class="chip">
+                    Pediatrics
+                    </div>
+                    <div class="chip">
+                    Physiotheraphy
+                </div>
+                    <div class="chip">
+                    Psychiatry
+                </div>
+                    <div class="chip">
+                    Psychology
+                </div>
+                    <div class="chip">
+                    Pulomonology
+                </div>
+                    <div class="chip">
+                    Radiology
+                </div>
+                    <div class="chip">
+                    Rheumatologist
+                </div>
+                    <div class="chip">
+                    Urology
+                </div>
+            </div>
+        </div>
         <footer class="page-footer">
           <div class="container">
             <div class="row">
@@ -42,6 +144,7 @@
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/js.js"></script>
     </body>
   </html>
 
---
+++ login.php
@@ -0,0 +1,48 @@
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/style.css" />
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <title>Doctor Mash - Find a Doctor</title>
+    </head>
+
+    <body>
+        <nav>
+            <div class="nav-wrapper">
+              <a href="#" class="brand-logo">Doctor Mash</a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Register.php">Register</a></li>
+              </ul>
+            </div>
+          </nav>
+        <br>
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Doctor Mash</h5>
+                <p class="grey-text text-lighten-4">We help you to find best doctor.</p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2016 Copyright Doctor Mash
+            <a class="grey-text text-lighten-4 right" href="https://github.com/iam-mhaseeb/doctormash">View Source</a>
+            </div>
+          </div>
+        </footer>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/js.js"></script>
+    </body>
+  </html>
+
